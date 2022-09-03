9663261337ff9d0f3e06d58d4c91744571c1e0b9
Thanakorn Wongklad <thanakorn.wong@kmutt.ac.th>
2022-09-04 00:54:30 +0700

    firstCommit

--- fillStartWord.js
+++ fillStartWord.js
@@ -1,6 +1,12 @@
 const { template } = require('@babel/core')
 
 function fillStartWord(startWord, word) {
-  //code here
+  console.log(startWord + word)
+  if (word == null || word == undefined ) {
+    console.log('undefined')
+  }else if (word[0,3]=startWord){
+    console.log(word)
+  }
+
 }
 module.exports = fillStartWord

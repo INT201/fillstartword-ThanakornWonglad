289f81b55c11578991429b24d9c9ce53bae0b2c4
Thanakorn Wongklad <thanakorn.wong@kmutt.ac.th>
2022-09-04 01:25:53 +0700

    scondCommit

--- fillStartWord.js
+++ fillStartWord.js
@@ -1,12 +1,17 @@
 const { template } = require('@babel/core')
 
 function fillStartWord(startWord, word) {
-  console.log(startWord + word)
-  if (word == null || word == undefined ) {
-    console.log('undefined')
-  }else if (word[0,3]=startWord){
-    console.log(word)
-  }
+  let str1 = String(startWord)
+  let str2 = String(word)
 
+  if (word == null || word == undefined) {
+    return 'undefined'
+  } else if (str2.substring(0,str1.length) == str1) {
+    return word
+  } else{
+    return str1 + str2
+  }
 }
+
+
 module.exports = fillStartWord
